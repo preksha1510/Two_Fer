24a19a28d4962af9b5c253ab29bbd9e7e0a189ec
Preksha Rai <preksharaib@gmail.com>
2023-06-30 18:26:20 +0530

    committed

---
+++ src/main/java/Twofer.java
@@ -0,0 +1,8 @@
+import static java.util.Objects.isNull;
+
+public class Twofer {
+    public String twofer(String name) {
+        name = isNull(name)? "you" : name;
+        return "One for " + name + ", one for me.";
+    }
+}
